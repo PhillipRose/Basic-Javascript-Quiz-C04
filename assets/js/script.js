5368682aad99601d61d2881d5d84b47c640547b9
Phillip R <philliprosejob@gmail.com>
2022-07-16 21:50:27 -0600

    slight tweaks to html and all comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-function init(){
-    console.log('js file hitting');
-};
-init();
+// function init(){
+//     console.log('js file hitting');
+// };
+// init();
 
 
 // put questions into objects with this formatting 
@@ -18,16 +18,28 @@ init();
 // var timer = 53;
 // how to make sure the time /5 is always just the whole number rounded down
 // var rounding = Math.trunc(timer/5)
-// this ruturns 10
+// this returns 10
 
 
 
 
-// TODO: make a timer with setInterval() and a timerCount variable 
+// TODO: make a timer with setInterval() and a timerCount variable
+
+
 // TODO:  append the question object content to the html
+
+
 // TODO:  set the various parts of the HTML to display:hidden or visibility: none check CSS styling options
+
+
 // TODO:  function to handle checking if the answer is correct or not and respond accordingly and increase score/decrease timer 
-// TODO:  function to clear the content from the html when a choice is selected.
+
+
+// TODO:  function to clear the content from the html when a choice is selected and pull up the next question.
+
+
 // TODO:  function to trigger game over when timer hits 0 or when all the questions are answered
+
+
 // TODO: function to use localStorage() set and get items to save scores 
-// TODO: function to change timer color when it gets low enough 
+
